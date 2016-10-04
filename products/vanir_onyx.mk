5b6231610259fe2211a4eb357ab0f896fe284844
DHOMD <DHO@dho.im>
2016-10-04 09:14:27 -0400

    Onyx The forgotten one?
    Apparently we should have been supporting Onyx for months?  A Year?  So let's add a mk
    
    Change-Id: I589f90b4b70943fb848c1af6c897647395392ce6

---
+++ products/vanir_onyx.mk
@@ -0,0 +1,25 @@
+#Squisher Choosing
+DHO_VENDOR := vanir
+
+PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-onyx
+
+# Boot Animation
+PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit device configuration
+$(call inherit-product, device/oneplus/onyx/device.mk)
+$(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
+
+# Inherit common onyx files.
+$(call inherit-product, vendor/vanir/products/common_phones.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := vanir_onyx
+PRODUCT_DEVICE := onyx
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := OnePlus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := onyx
