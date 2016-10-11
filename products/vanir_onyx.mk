29789dfb8ecdd187455011c6c8819b7d01b74ffc
DHOMD <DHO@dho.im>
2016-10-11 19:52:13 -0400

    FIx Onyx boot
    credit Izwan M
    
    Change-Id: I243e01e46d24600fb95c4b85a89c205f276c0a30

--- products/vanir_onyx.mk
+++ products/vanir_onyx.mk
@@ -7,7 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=vanir-onyx
 PRODUCT_COPY_FILES += vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
-$(call inherit-product, device/oneplus/onyx/device.mk)
+$(call inherit-product, device/oneplus/onyx/full_onyx.mk)
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
 # Inherit common onyx files.
